09ad4ef15617e67441b8727e6a1054b0213bca49
torres-leo <leojts24@gmail.com>
2022-07-11 19:45:56 -0600

    finish contact section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -170,7 +170,7 @@ const AboutMe = () => {
 					</p>
 					<p className='mt-8 text-center lg:text-start text-gray-400 tracking-wide text-xl mb-6'>
 						Also, I like to learn about new things for the web.{' '}
-						<span className='block text-xl text-gray-400'>¡Never stop Learning! ;)</span>
+						<span className='block text-xl text-gray-400'>¡ Never stop Learning ! ;)</span>
 					</p>
 					<div className='block text-left my-6 text-lg'>
 						<p className='text-gray-400'>These are a few technologies I've been working with recently:</p>
